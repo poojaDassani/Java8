fff2bedd3b5458740b3e1d46514f95b3ec1e3459
Pooja Dassani <poojadassani2811@gmail.com>
2024-02-20 23:42:53 +0530

    LinkedListEasyExampleQuestions

--- src/LeetCode/LinkedListEasyExampleQuestions.java
+++ src/LeetCode/LinkedListEasyExampleQuestions.java
@@ -4,7 +4,16 @@ import LeetCode.Models.ListNode;
 
 public class LinkedListEasyExampleQuestions {
     public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
 
+        ListNode nodeToDelete = head.next.next;
+
+        //Delete Node in a Linked List
+        deleteNode(nodeToDelete);
     }
 
     public static void deleteNode(ListNode node) {
